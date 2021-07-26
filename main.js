28eae6fcff342a23fb71280ee30af209bdbf2082
Olga Osipchuk <olgaosipchuk@MacBook-Pro.local>
2021-07-25 21:34:32 -0700

    updating lab

---
+++ main.js
@@ -0,0 +1,24 @@
+function addToDo(evt) {
+    if ($("input").val() !== "") {
+        $("ul").append($(`<li><button class="delete">X</button><span class="listItem">${$("input").val()}</span></li>`));
+        $("input").val("");
+    };
+    deleteSkill();
+
+
+}
+
+
+
+function deleteSkill() {
+    $(".delete").click(function () {
+        $(this).closest('li').fadeOut(
+            1000
+            , function () {
+                $(this).remove();
+            }
+        );
+    })
+}
+
+$("#addSkills").click(addToDo)
